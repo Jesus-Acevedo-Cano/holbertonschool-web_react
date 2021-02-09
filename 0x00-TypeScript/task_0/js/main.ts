f02e04e4bdcc9027a686218f627f8442e928f515
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-02-09 16:53:02 -0500

    vanilla js to render table

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -20,3 +20,37 @@ const student_2: Student = {
 };
 
 const studentsList: Array<Student> = [student_1, student_2];
+
+/* <Using Vanilla JS to render a table> */
+
+const body: HTMLBodyElement = document.getElementsByTagName("body")[0];
+const table: HTMLTableElement = document.createElement('table');
+const thead: HTMLTableSectionElement = document.createElement('thead');
+const tbody: HTMLTableSectionElement = document.createElement('tbody');
+const tr: HTMLTableRowElement = document.createElement('tr');
+const th1: HTMLTableCellElement = document.createElement('td');
+const th2: HTMLTableCellElement = document.createElement('td');
+
+th1.innerHTML = 'firstName';
+th2.innerHTML = 'location';
+
+body.append(table);
+table.append(thead);
+table.append(tbody)
+thead.append(tr);
+tr.append(th1);
+tr.append(th2);
+
+studentsList.forEach(element => {
+  const trTb: HTMLTableRowElement = document.createElement('tr');
+  tbody.append(trTb);
+
+  let td: HTMLTableCellElement = document.createElement('td');
+  td.innerHTML = element.firstName;
+  trTb.append(td);
+
+  td = document.createElement('td');
+  td.innerHTML = element.location;
+  trTb.append(td);
+});
+/* <Using Vanilla JS to render a table/> */
