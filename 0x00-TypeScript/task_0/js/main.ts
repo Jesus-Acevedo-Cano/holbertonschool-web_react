edf8e4997bd9343c963c42114b5f71c11cb8ea40
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-02-09 16:33:06 -0500

    init files

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,21 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student_1: Student = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+  age: 30,
+  location: 'Miami',
+};
+
+const student_2: Student = {
+  firstName: 'Naomi',
+  lastName: 'Sorell',
+  age: 30,
+  location: 'Ohio',
+};
+
