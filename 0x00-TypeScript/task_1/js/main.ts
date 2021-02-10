2f2059379f3ecd80c67e077d73633871d8b0f2e5
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-02-09 23:50:08 -0500

    task 4 class constructor

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -22,3 +22,30 @@ interface printTeacherFunction {
 }
 
 export const printTeacher: printTeacherFunction = printTeacher_;
+
+/* task 4 class construction */
+export class StudentClass implements Student {
+  firstName: string;
+  lastName: string;
+  constructor(firstName:string, lastName:string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework() {
+    return 'Currently working';
+  }
+  displayName() {
+    return this.firstName;
+  }
+}
+
+export interface Student {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export interface StudentConstructor {
+  new(firstName: string, lastName: string): Student;
+}
