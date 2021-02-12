b2f0fea711ab3aec9417a27ca48ebc77a60fc918
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-02-12 18:41:01 -0500

    task 1 lodash debounce

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,17 @@
+import $ from 'jquery';
+const _ = require('lodash');
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append(`<button type='button'>Click here to get started</button>`);
+$('body').append(`<p id='count'></p>`);
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let clicks = 0;
+
+const updateCounter = function() {
+  clicks++;
+  $('#count').text(`${clicks} clicks on the button`);
+
+}
+$('button').on('click', _.debounce(updateCounter));
