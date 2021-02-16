ba1e1e063b7e22b78cf1c9322c110601afdb3298
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-02-15 23:07:52 -0500

    upload content task 2 0x01-webpack css and images

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -3,19 +3,35 @@ const path = require("path");
 module.exports = {
   mode: 'production',
   entry: './js/dashboard_main.js',
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public")
   },
-  module : {
+  module: {
     rules: [
-      { 
-        test: /\.css$/, 
-        use: ["style-loader", "css-loader"] 
-      },
-      { 
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }, 
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
       },
     ]
   }
