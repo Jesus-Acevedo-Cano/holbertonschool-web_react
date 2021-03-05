f71b7f59a8f7cbd6924f71d0bbe710276ffb68b8
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-03-05 02:06:09 -0500

    init files react components project

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -8,30 +8,30 @@ import CourseList from '../CourseList/CourseList';
 import PropTypes from 'prop-types';
 
 class App extends Component {
-  render () {
-  const listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 }
-  ];
+  render() {
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 }
+    ];
 
-  const listNotifications = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: { __html: getLatestNotification()} }
-  ];
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
+    ];
 
-  return (
-    <React.Fragment>
-      <Notifications listNotifications={ listNotifications }/>
-      <div className="App">
-        <Header/> 
-        { this.props.isLoggedIn ? <CourseList listCourses={ listCourses }/> : <Login /> }
-        <Footer/>
-      </div>
-    </React.Fragment>
-  );
-}
+    return (
+      <React.Fragment>
+        <Notifications listNotifications={listNotifications} />
+        <div className="App">
+          <Header />
+          {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 App.propTypes = {
