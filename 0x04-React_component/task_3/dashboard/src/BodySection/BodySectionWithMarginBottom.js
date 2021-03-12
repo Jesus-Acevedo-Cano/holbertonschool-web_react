fadf630b5ef57c96cdccb01bc0d0f8f1558761df
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-03-12 03:03:22 -0500

    task 3 to 6 react components, footer fixed

---
+++ 0x04-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import BodySection from './BodySection';
+import './BodySectionWithMarginBottom.css';
+import PropTypes from 'prop-types';
+
+class BodySectionWithMarginBottom extends Component {
+  render() {
+    return (
+      <div className='bodySectionWithMargin'>
+        <BodySection title={this.props.title} children={this.props.children} />
+      </div>
+    );
+  }
+}
+
+BodySectionWithMarginBottom.propTypes = {
+  title: PropTypes.string
+}
+BodySectionWithMarginBottom.defaultProps = {
+  title: ''
+}
+export default BodySectionWithMarginBottom;
---
+++ 0x04-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import BodySectionWithMarginBottom from './BodySectionWithMarginBottom'
+import BodySection from './BodySection'
+
+describe('Test BodySectionWithMarginBottom.js', () => {
+  it('Render without crashing', () => {
+    expect(shallow(<BodySectionWithMarginBottom title='test title' />).exists());
+  });
+
+  it ('correctly render of BodySection component and props are passed correctly to the child component', () => {
+    const wrapper = shallow(<BodySectionWithMarginBottom title='test title'><p>test children node</p></BodySectionWithMarginBottom>)
+    expect(wrapper.contains(<div className='bodySectionWithMargin' />));
+    expect(wrapper.children()).to.have.lengthOf(1);
+    expect(wrapper.find(BodySection)).to.have.lengthOf(1);
+    expect(wrapper.find(BodySection).children()).to.have.lengthOf(1);
+    expect(wrapper.find(BodySection).props().title).to.equal('test title');
+    expect(wrapper.find('p')).to.have.lengthOf(1);
+    expect(wrapper.find('p').text()).to.equal('test children node');
+  })
+});
