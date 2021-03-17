fe753aed6658ba1a51d8df200ce3ac8d846c3270
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-03-16 22:24:31 -0500

    upload files advances immutable

--- 0x07-React_Immutable/5-merge.js
+++ 0x07-React_Immutable/5-merge.js
@@ -1,5 +1,5 @@
 import Immutable from 'immutable';
 
-export const concatElements = (page1, page2) => Immutable.List(page1).concat(immutable.List(page2));
+export const concatElements = (page1, page2) => Immutable.List(page1).concat(Immutable.List(page2));
 
-export const mergeElements = (page1, page2) => Immutable.Map(page1).merge(immutable.Map(page2));
+export const mergeElements = (page1, page2) => Immutable.Map(page1).merge(Immutable.Map(page2));
