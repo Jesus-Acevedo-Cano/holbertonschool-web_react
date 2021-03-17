dc0eb16ab8055b0dda6a1ed29f8ead83c1e34ea3
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-03-16 21:46:36 -0500

    upload files advances immutable

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -3,6 +3,6 @@ import Immutable from 'immutable';
 const accessImmutableObject = (object, array) => {
   const nestData = Immutable.fromJS(object);
   return nestData.getIn(array, undefined);
-}
+};
 
 export default accessImmutableObject;
